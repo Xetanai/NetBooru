ebcaad137135ab4b2c6f619954ee92f4dbf71177
FiniteReality <digpoealternate@gmail.com>
2020-05-25 18:21:54 +0100

    Add DisplayLandingPage option

---
+++ src/Web/Options/LandingOptions.cs
@@ -0,0 +1,14 @@
+namespace NetBooru.Web.Options
+{
+    /// <summary>
+    /// Landing page options
+    /// </summary>
+    public class LandingOptions
+    {
+        /// <summary>
+        /// Gets or sets a value indicating whether to display the landing
+        /// page, or redirect to the posts page.
+        /// </summary>
+        public bool DisplayLandingPage { get; set; }
+    }
+}
--- src/Web/Startup.cs
+++ src/Web/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using NetBooru.Data;
+using NetBooru.Web.Options;
 
 namespace NetBooru.Web
 {
@@ -28,6 +29,9 @@ namespace NetBooru.Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            _ = services.Configure<LandingOptions>(
+                Configuration.GetSection("Landing"));
+
             var connectionString =
                 Configuration.GetConnectionString("Database");
 
